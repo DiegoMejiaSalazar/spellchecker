575d8ad3f8d278489ed58be4fe34de0955f7084d
DiegoMejia <doms1369@gmail.com>
2022-09-19 09:39:39 -0400

    dccuchile

--- app.py
+++ app.py
@@ -7,23 +7,34 @@ from transformers import BertForMaskedLM, BertTokenizer
 from waitress import serve
 import sys
 
-tokenizer = BertTokenizer.from_pretrained('Diegomejia/bert-ucb-5')
-model = BertForMaskedLM.from_pretrained('Diegomejia/bert-ucb-5')
+tokenizer = BertTokenizer.from_pretrained('dccuchile/bert-base-spanish-wwm-cased')
+model = BertForMaskedLM.from_pretrained('dccuchile/bert-base-spanish-wwm-cased')
 app = Flask(__name__)
 
 
 @app.route('/spell-check', methods = ["POST"])
 def spell_check():
-    if 'file' not in request.files:
-        return 'No file part', HTTPStatus.BAD_REQUEST
-    bibliography_index = int(request.args.to_dict().get('bibliographystart'))
-    figure_index = int(request.args.to_dict().get('figureIndexEndPage'))
-    general_index = int(request.args.to_dict().get('generalIndexEndPage')) 
-    table_index = int(request.args.to_dict().get('tableIndexEndPage'))
-    if bibliography_index == 0:
-        bibliography_index = sys.maxsize
-    max_index_page = max([figure_index, general_index, table_index]) + 1
-    return {"errors": analize_file_with_bert(request.files['file'], model, tokenizer, bibliography_index, max_index_page)}, HTTPStatus.OK
+    try:
+        if 'file' not in request.files:
+            return 'No file part', HTTPStatus.BAD_REQUEST
+        bibliography_index = int(request.args.to_dict().get('bibliographystart'))
+        figure_index = int(request.args.to_dict().get('figureIndexEndPage'))
+        general_index = int(request.args.to_dict().get('generalIndexEndPage')) 
+        table_index = int(request.args.to_dict().get('tableIndexEndPage'))
+        if bibliography_index == 0:
+            bibliography_index = sys.maxsize
+        max_index_page = max([figure_index, general_index, table_index]) + 1
+        return {"errors": analize_file_with_bert(request.files['file'], model, tokenizer, bibliography_index, max_index_page)}, HTTPStatus.OK
+    except:
+        print("there was an error")
+        print("bibliography index ", bibliography_index)
+        print("figure_index ", figure_index)
+        print("general_index ", general_index)
+        print("table_index ", table_index)
+        print("max_index_page ", max_index_page)
+        return {"errors": []}, HTTPStatus.OK
+    
+
 
 if __name__ == '__main__':
     EnglishDictionary.load()
